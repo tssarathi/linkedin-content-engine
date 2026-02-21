536f4697100e135f8933a6d50012a2ed0272fd96
Sarathi Thirumalai Soundararajan <tssarathi5901@gmail.com>
2026-02-21 19:34:09 +1100

    fix(config): correct spelling of 'Analyser' in configuration comments

--- shared/config.py
+++ shared/config.py
@@ -1,11 +1,12 @@
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class Config:
-    # -- GitHub Analyzer --
+    # -- GitHub Analyser --
     RA_GA_GROQ_API_KEY = os.getenv("RA_GA_GROQ_API_KEY")
     RA_GA_GITHUB_API_KEY = os.getenv("RA_GA_GITHUB_API_KEY")
 
