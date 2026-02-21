c6f9bcad69d5763bcfdc0765ed8d508612862e83
Sarathi Thirumalai Soundararajan <tssarathi5901@gmail.com>
2026-02-21 11:44:29 +1100

    chore(setup): add setup and config files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="researcher-agent",
+    version="1.0.0",
+    author="Sarathi",
+    packages=find_packages(),
+    install_requires=requirements,
+)
